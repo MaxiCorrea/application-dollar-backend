9dc79ee1a73a90dfacaa2a26c3eb6f56bc4f9e9e
MaxiCorrea <jmaxicorrea@gmail.com>
2024-04-01 02:17:51 -0300

    Added CORS

--- src/main/java/com/maxicorrea/github/applicationdollarbackend/ApplicationDollarBackendApplication.java
+++ src/main/java/com/maxicorrea/github/applicationdollarbackend/ApplicationDollarBackendApplication.java
@@ -2,12 +2,25 @@ package com.maxicorrea.github.applicationdollarbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApplicationDollarBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationDollarBackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationDollarBackendApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+      }
+    };
+  }
 
 }
